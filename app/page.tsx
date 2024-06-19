51445a66a03508ff343c803a767f83e064d61d7b
Rohit Tyagi <rohit.tyagi@thoughts2binary.com>
2024-06-19 13:48:21 +0530

    dsfs

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,12 @@ import { useAuth } from "@/zstore";
 import { useRouter } from "next/navigation";
 
 export default function Home() {
-  // const { user } = useAuth();
-  // const router = useRouter();
-  // if (user._id) router.replace(`/main/${user.org}`);
-  // else {
-  //   router.replace(`/login`);
-  // }
+  const { user } = useAuth();
+  const router = useRouter();
+  if (user._id) router.replace(`/main/${user.org}`);
+  else {
+    router.replace(`/login`);
+  }
 
   return null;
 }
