81ac6303a2b43140549b41d7f2042e32717dd144
Rohit Tyagi <rohit.tyagi@thoughts2binary.com>
2024-06-24 10:18:08 +0530

    connection logging

--- hooks/useRealtimeChannels.tsx
+++ hooks/useRealtimeChannels.tsx
@@ -1,5 +1,6 @@
 "use client";
 import Ably from "ably";
+import { useEffect } from "react";
 
 type useRealtimeChannelsProps = {};
 
@@ -7,6 +8,16 @@ const ablyClient = new Ably.Realtime(process.env.NEXT_PUBLIC_ABLY_KEY || "");
 
 const useRealtimeChannels = () => {
   const notificationChannel = ablyClient.channels.get("NOTIFICATION");
+
+  useEffect(() => {
+    ablyClient.connection.on("connected", () => {
+      console.log("Connected to Ably!");
+    });
+    ablyClient.connection.on("closed", () => {
+      console.log("Ably CLOSED!");
+    });
+    return () => ablyClient.close();
+  }, []);
 
   return { notificationChannel };
 };
