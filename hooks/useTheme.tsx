fbb8c096368a07ab6bef4279fff67d1530aef44b
Rohit Tyagi <rohit.tyagi@thoughts2binary.com>
2024-06-28 10:05:18 +0530

    type fix

--- hooks/useTheme.tsx
+++ hooks/useTheme.tsx
@@ -8,40 +8,44 @@ const useTheme = () => {
   const { pulseHeight: configPulseHeight, themeID: configThemeID } =
     useConfig();
   //
-  const applyTheme = useCallback(() => {
-    const localPulseHeight = localStorage.getItem("pulse-height");
-    const localTHemePk = localStorage.getItem("theme-preference") || "";
+  const applyTheme = useCallback(
+    (externalThemeID?: string) => {
+      const localPulseHeight = localStorage.getItem("pulse-height");
+      const localTHemePk =
+        externalThemeID || localStorage.getItem("theme-preference") || "";
 
-    const root = document.documentElement;
-    const [
-      main_bg,
-      main_fg,
-      border_light,
-      highlighter,
-      highlight_dark,
-      text_color,
-    ] =
-      themeConfig.find((t) => t._id === localTHemePk)?.colors ||
-      themeConfig[0].colors;
+      const root = document.documentElement;
+      const [
+        main_bg,
+        main_fg,
+        border_light,
+        highlighter,
+        highlight_dark,
+        text_color,
+      ] =
+        themeConfig.find((t) => t._id === localTHemePk)?.colors ||
+        themeConfig[0].colors;
 
-    const pulseHeight =
-      (localPulseHeight as PulseHeightType) || configPulseHeight || "sm";
+      const pulseHeight =
+        (localPulseHeight as PulseHeightType) || configPulseHeight || "sm";
 
-    localStorage.setItem("theme-preference", localTHemePk);
-    localStorage.setItem("pulse-height", pulseHeight);
+      localStorage.setItem("theme-preference", localTHemePk);
+      localStorage.setItem("pulse-height", pulseHeight);
 
-    root.style.setProperty("--main-bg", main_bg);
-    root.style.setProperty("--main-fg", main_fg);
-    root.style.setProperty("--border-light", border_light);
-    root.style.setProperty("--highlighter", highlighter);
-    root.style.setProperty("--highlighter-dark", highlight_dark);
-    root.style.setProperty("--text-color", text_color);
-    //
-    root.style.setProperty(
-      "--pulse-height",
-      pulseHeightConfig[pulseHeight] as PulseHeightType
-    );
-  }, [configThemeID, configPulseHeight]);
+      root.style.setProperty("--main-bg", main_bg);
+      root.style.setProperty("--main-fg", main_fg);
+      root.style.setProperty("--border-light", border_light);
+      root.style.setProperty("--highlighter", highlighter);
+      root.style.setProperty("--highlighter-dark", highlight_dark);
+      root.style.setProperty("--text-color", text_color);
+      //
+      root.style.setProperty(
+        "--pulse-height",
+        pulseHeightConfig[pulseHeight] as PulseHeightType
+      );
+    },
+    [configThemeID, configPulseHeight]
+  );
 
   const applyPulseHeight = useCallback((h: PulseHeightType) => {
     const root = document.documentElement;
